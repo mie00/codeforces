500d20570e24b055dbc7c41190c562e989037161
Mohamed Elawadi <mohamed@elawadi.net>
2019-10-24 04:24:24 +0200

    new error handling

--- new.go
+++ new.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"path"
@@ -8,13 +9,20 @@ import (
 
 func newFile(config Config) error {
 	target := path.Join(config.Name, config.Filename)
-	if _, err := os.Stat(target); !config.ForceDownload && err == nil {
-		return nil
-	} else if config.ForceDownload || os.IsNotExist(err) {
-		return Copy(path.Join("templates", config.Filename), target)
+	if _, err := os.Stat(target); !config.ForceWrite && err == nil {
+		return fmt.Errorf("file %s already exists", target)
+	} else if config.ForceWrite || os.IsNotExist(err) {
 	} else {
 		return err
 	}
+	src := path.Join("templates", config.Filename)
+	if _, err := os.Stat(src); err == nil {
+	} else if os.IsNotExist(err) {
+		return fmt.Errorf("cannot find template %s", src)
+	} else {
+		return err
+	}
+	return Copy(src, target)
 }
 
 func Copy(src, dst string) error {
